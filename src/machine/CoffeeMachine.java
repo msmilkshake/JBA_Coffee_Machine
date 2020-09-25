68756d8921216efd27bec0bad8e5bbf18be20aca
Paulo Filipe <prnfilipe@gmail.com>
2020-09-25 15:35:59 +0100

    Stage 3 completed

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -10,12 +10,33 @@ public class CoffeeMachine {
     private static final Scanner scn = new Scanner(System.in);
     
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need:");
+        System.out.println("Write how many ml " +
+                "of water the coffee machine has:");
+        int avlbWater = scn.nextInt();
+        System.out.println("Write how many ml of " +
+                "milk the coffee machine has:");
+        int avlbMilk = scn.nextInt();
+        System.out.println("Write how many grams of " +
+                "coffee beans the coffee machine has:");
+        int avlbBeans = scn.nextInt();
+        System.out.println("Write how many " +
+                "cups of coffee you will need:");
         int numOfCoffees = scn.nextInt();
-        System.out.println("For " + numOfCoffees +
-                " cups of coffee you will need:\n" +
-                numOfCoffees * WATER + " ml of water\n" +
-                numOfCoffees * MILK + " ml of milk\n" +
-                numOfCoffees * BEANS + " g of coffee beans");
+        
+        int possibleCoffees = Math.min(Math.min(
+                avlbWater / WATER,
+                avlbMilk / MILK),
+                avlbBeans / BEANS);
+        
+        if (possibleCoffees < numOfCoffees) {
+            System.out.println("No, I can make only " +
+                    possibleCoffees + " cup(s) of coffee");
+        } else if (possibleCoffees == numOfCoffees) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            System.out.println("Yes, I can make that amount of coffee " +
+                    "(and even " + (possibleCoffees - numOfCoffees) +
+                    " more than that)");
+        }
     }
 }
